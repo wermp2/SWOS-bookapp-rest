ba767d50b25a7775ea0236df0c6bffeae560fd2a
wermp2 <wermp2@bfh.ch>
2015-06-16 18:05:00 +0200

    added test

---
+++ ch.bfh.swos.bookapp.service/src/test/java/ch/bfh/swos/bookapp/service/SimpleUnitTestService.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.bfh.swos.bookapp.service;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Pixel
+ */
+public class SimpleUnitTestService {
+    
+    @Test
+    public void simpleTestService()
+    {
+        boolean test1 = false;
+        boolean test = (1+1) == 2;
+        Assert.assertTrue(test1);
+    }
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}

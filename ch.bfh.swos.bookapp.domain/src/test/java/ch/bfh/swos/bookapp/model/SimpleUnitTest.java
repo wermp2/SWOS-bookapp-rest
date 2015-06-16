44ba50f9173d483901f0f82c09992e9b72d42927
wermp2 <wermp2@bfh.ch>
2015-06-16 17:51:34 +0200

    test2...

---
+++ ch.bfh.swos.bookapp.domain/src/test/java/ch/bfh/swos/bookapp/model/SimpleUnitTest.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.bfh.swos.bookapp.model;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Pixel
+ */
+public class SimpleUnitTest {
+    
+    @Test
+    public void simpleTestMeth()
+    {
+        boolean test1 = true;
+        boolean test = (1+1) == 2;
+        Assert.assertTrue(test1);
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
